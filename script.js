f5684618ffe15b1284f1460518f83fd08c3a7bdd
akalampv <dev.akalampv@gmail.com>
2021-10-03 21:43:09 +0300

    Edit script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.querySelector("#count");
+const total = document.querySelector("#total");
+const movieSelect = document.querySelector("#movie");
+
+let ticketPrice = Number(movieSelect.value);
+
+// Update total and count
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll(".row .seat.selected");
+    const selectedSeatsCount = selectedSeats.length;
+    count.innerHTML = String(selectedSeatsCount);
+    total.innerHTML = String(selectedSeatsCount * ticketPrice);
+}
+
+// Movie select event
+movieSelect.addEventListener("change", (e) => {
+    ticketPrice = Number(e.target.value);
+    updateSelectedCount();
+});
+
+// Seat click event
+container.addEventListener("click", (e) => {
+    if (e.target.classList.contains("seat") && !e.target.classList.contains("occupied")) {
+        e.target.classList.toggle("selected");
+        updateSelectedCount();
+    }
+});
